6f0fcfcd519dbff7f2cf2a167469c7bb6fabedeb
Виктор Решетов <vityareshetov@yandex.ru>
2020-05-03 10:34:52 +0300

    Поправил мелкие косяки

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ class Folder {
         this.innerFiles = {};
         if (!this.node)
             return;
-​
+
         for (let child of this.node.children) {
             if (child.classList.contains('file')) {
                 let key = child.querySelector('span').innerText;
@@ -17,41 +17,40 @@ class Folder {
                 this.innerFolders[key] = new Folder(child);
             }
         }
-​
     }
-​
+
     handleClick(event) {
         const parentNode = event.target.parentNode;
         const targetNode = parentNode.querySelector('ul');
         if (!targetNode)
             return;
-​
+
         if (targetNode.classList.contains('hidden')) {
             targetNode.classList.remove('hidden');
         } else {
             const childrenContainers = parentNode.querySelectorAll('ul');
             if (!childrenContainers)
                 return;
-​
+
             childrenContainers.forEach(container => { container.classList.add('hidden') });
         }
     }
-​
+
     addFile(fileName) {
         if (this.innerFiles[fileName])
             return;
-​
+
         let newFile = document.createElement('li');
         newFile.innerHTML = `<span>${fileName}</span>`;
         newFile.classList.add('file');
         this.node.append(newFile);
         this.innerFiles[fileName] = newFile;
     }
-​
+
     addFolder(name) {
         if (this.innerFolders[name])
             return;
-​
+
         let newFolder = document.createElement('li');
         newFolder.classList.add('folder');
         newFolder.innerHTML = `<span>${name}</span><ul></ul>`;
@@ -59,26 +58,26 @@ class Folder {
         this.innerFolders[name] = new Folder(newFolder);
     }
 }
-​
+
 function handleButtonClick(root) {
     const input = document.querySelector('input');
     if (!input.value || input.value.length === 0)
         return;
-​
+
     let path = input.value.split('/');
     if (path.length < 3 || root.name !== path[1])
         return;
-​
+
     const fileName = path.slice(path.length - 1)[0];
     path = path.slice(1, length - 1);
     if (!fileName.includes('.'))
         return;
-​
+
     let currentFolder = root;
     for (let i = 0; i < path.length - 1; i++) {
         if (currentFolder.name !== path[i])
             return; // бан
-    ​
+
         if (!currentFolder.innerFolders[path[i + 1]]) {
             // снова можно создать директорию, но нужно ли?
             currentFolder.addFolder(path[i + 1]);
@@ -91,3 +90,9 @@ function handleButtonClick(root) {
     currentFolder.addFile(fileName)
 }
 
+function init() {
+    const root = new Folder(document.querySelector('.root'));
+    document.querySelector('button').addEventListener('click', handleButtonClick.bind(null, root))
+}
+
+init();
